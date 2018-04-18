b400d1053796ffdb576921a2d646517e679a4044
Adrien21 <38344498+Adrien21@users.noreply.github.com>
2018-04-18 17:05:21 +0200

    Update bdd_connect.php

--- bdd_connect.php
+++ bdd_connect.php
@@ -1,7 +1,7 @@
 <?php
 try {
 	$dbconnect = new PDO('mysql:host=localhost;dbname='.$mysql_db, $mysql_user, $mysql_pass, array(PDO::ATTR_PERSISTENT => true));
-};
+}
 catch (PDOException $e) {
 	print "Erreur : " . $e->getMessage() . "<br/>";
 	die();
