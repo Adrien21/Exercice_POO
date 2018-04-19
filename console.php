a190f4e92ddd9bdabee5f58d5f5d52b1f691a8ea
Adrien21 <38344498+Adrien21@users.noreply.github.com>
2018-04-19 15:07:11 +0200

    Update console.php
    Simplification avec un __get pour remplacer tous les GETs

--- console.php
+++ console.php
@@ -1,5 +1,5 @@
 <?php
-/*class console{
+class console{
 	//const NOM = "PS1", CONSTRUCTEUR = "Sony", DATESORTIE = "19941203";
 	private $id, $nom, $constructeur, $prix, $dateSortie;
 
@@ -8,9 +8,14 @@
 		$this->nom = $nom;
 		$this->constructeur = $constructeur;
 		$this->prix = $prix;
-		$this->datesortie = $dateSortie;
+		$this->dateSortie = $dateSortie;
     }
 	//getter
+	public function __get($variable){
+		if($variable != "id"): return $this->$variable;
+		endif;
+	}
+	/*
 	public function getId(){
 		return $this->id;
 	}
@@ -23,9 +28,10 @@
 	public function getPrix(){
 		return $this->prix;
 	}
-	public function getDatesortie(){
-		return $this->datesortie;
+	public function getDateSortie(){
+		return $this->dateSortie;
 	}
+*/
 	//setter
 	public function setNom($newNom){
 		return $this->nom = $newNom;
@@ -39,39 +45,49 @@
 		$this->prix = $newPrix;
 		endif;
 	}
-	public function setDatesortie($newDatesortie){
+	public function setDatesortie($newDateSortie){
 		//verifier que date de sortie est au format date
-		
-		return $this->datesortie = $newDatesortie;
+		return $this->dateSortie = $newDateSortie;
 	}
 }
-*/
+
 //info bdd
 
 //connexion a la bdd
 include('bdd_connect.php');
 
 //nouvel objet connexion
-$db = new bddConnect('ma');
+$db = new bddConnect();
 
 //envoi la requete a l'objet connexion
-$data = $db->query('SELECT * from console') ;
+//$datas = $db->query('UPDATE `console` SET `nom` = "ps1" WHERE `console`.`id` = 1') ;
+$datas = $db->query('SELECT * FROM console') ;
 //affiche le resultat
-var_dump ($data);
+var_dump ($datas);
 
 //parcours les objets du resultat et en affiche chaque elements
-foreach($db->query('SELECT * from console') as $data){
+foreach($datas as $data){
 	echo $data->id."<br />";
 	echo $data->nom."<br />";
 	echo $data->constructeur."<br />";
-	echo $data->prix."<br />";
+	echo $data->prix." €<br />";
 	echo $data->dateSortie."<br />";
 	echo "<br />";
 	
 	$data->prix = 10;
-	echo $data->prix."<br />";
+	//echo $data->prix."<br />";
 };
 
 
+$PS = new console(1, "PS1", "Sony", 12, "19941203");
+echo $PS->nom."<br />";;
+echo $PS->id."<br />";;
+echo $PS->prix."<br />";;
+$PS->setPrix(10);
+$PS->autre = 9;
+//$PS->prix = 9;
+echo $PS->autre."<br />";;
+
+var_dump($PS);
 
 ?>
