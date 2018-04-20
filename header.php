5f5f117c8a3d028006df43d503c469a409f029c0
Léopold Assogba <leopoldassogba21@gmail.com>
2018-04-20 12:43:17 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ header.php
@@ -0,0 +1,27 @@
+<?php 
+session_start(); 
+
+
+/*  si on a besoin de rajouter du php avant  */
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <link rel="stylesheet" href="NOTRE CSS OPTIONNEL">
+        <meta charset="utf-8">
+        <meta name="author" content="Adrien Arnaud Henry Eric Léo Valentin">
+        <title> Site JV Exercice POO </title>
+    </head>
+    <body>
+        <header>
+        
+        
+        </header>
+        
+        
+
+
+
