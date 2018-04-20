2d0a1b276aacae3a2ef3cb7829757af3bb78553e
Unknown <unknown@example.com>
2018-04-20 14:08:43 +0200

    Revert "divers tests sur objet jeux"
    This reverts commit 94b1b2d47eefb657416818d65e9ba33a212eceb6.

--- jeudlc/jeux.php
+++ jeudlc/jeux.php
@@ -15,8 +15,6 @@ $db = new bddConnect($mysql_db, $mysql_user, $mysql_pass, $mysql_server);
 //appel objet jeu
 require_once("class_jeux.php");
 
-$newJeu = new jeudlc(NULL, 'jeu98', 'edit1', 'dev1', '2018-04-20', '15', '12', 'blablabla', '2');
-//$requete = "INSERT INTO `jeudlc` (`nom`, `editeur`, `dev`, `dateSortie`, `prix`, `pegi`, `description`, `idJeuParent`) VALUES ('jeu98', 'edit1', 'dev1', '2018-04-20', '15', '12', 'blablabla', '2')";
 //$requete = "UPDATE `jeudlc` SET `nom` = 'jeu1' WHERE `jeudlc`.`id` = 1";
 $requete = 'SELECT * from jeudlc';
 $dbrequete = $db->query($requete) ;
@@ -26,8 +24,7 @@ $db = null;
 
 //var_dump ($dbrequete);
 foreach($dbrequete as $row) {
-	$jeu = new jeudlc($row->id, $row->nom, $row->editeur, $row->dev, $row->dateSortie, $row->prix, $row->pegi, $row->description, $row->idJeuParent);//--PDO::FETCH_OBJ
-	//$jeu = new jeudlc($row["id"], $row["nom"], $row["editeur"], $row["dev"], $row["dateSortie"], $row["prix"], $row["pegi"], $row["description"]);//--PDO::FETCH_ASSOC
+	$jeu = new jeudlc($row->id, $row->nom, $row->editeur, $row->dev, $row->dateSortie, $row->prix, $row->pegi, $row->description);
 	echo $jeu->id."<br />";
 	echo $jeu->nom."<br />";
 	echo $jeu->editeur."<br />";
@@ -36,16 +33,20 @@ foreach($dbrequete as $row) {
 	echo $jeu->prix." euros<br />";
 	echo $jeu->pegi."<br />";
 	echo $jeu->description."<br />";
-	echo $jeu->idJeuParent."<br />";
 	echo "<hr /><br />";
 }
 
 //var_dump ($jeu);
 
-// //modification du prix du dernier jeu
+// //modification du prix du jeu
 echo "--- modification du prix du jeu ---<br /><br />";
 $jeu->setPrix(35)
 	->setPegi(10);
+// echo $ps1->id."<br />";
+// echo $ps1->nom."<br />";
+// echo $ps1->getConstructeur()."<br />";
+// echo $ps1->getPrix()." euros<br />";
+// echo $ps1->getDatesortie()."<br />";
 	echo $jeu->id."<br />";
 	echo $jeu->nom."<br />";
 	echo $jeu->editeur."<br />";
