d02fa39a159c46c4726e109642df86c757e5a415
Adrien21 <drolein@hotmail.com>
2018-04-25 12:36:51 +0200

    Auto stash before merge of "master" and "origin/master"
    mise a jour bdd_connect.php utf8
    supprimer sur objet console

--- SITE_FINAL/console/class_console.php
+++ SITE_FINAL/console/class_console.php
@@ -40,6 +40,7 @@ class console{
 	
 	//autres fonctions
 	public function display($connexion){
+		require_once('supprime/class_supprime.php');
 		//pour afficher jeux en fonction de la console choisi
 		$arrDisponible = $connexion->query('SELECT jeuDlc.nom FROM console JOIN lien ON lien.idconsole = console.id JOIN jeuDlc ON jeuDlc.id = lien.idJeuDlc WHERE console.id = "'.$this->id.'"');
 		//var_dump($arrDisponible);
@@ -58,7 +59,7 @@ class console{
 				}
 				echo substr($strDpb, 0, -2);
 			}
-			
+			$affichche_suppr = new supprime($this->id, $this->nom, "console");
 			echo '</p></article>';
 	}
 }
